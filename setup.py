21fa9cbe97e8d30a93d571cbd01776f5ff5139bc
Vivek Kumar <91729976+vivek-kumar85@users.noreply.github.com>
2023-04-08 19:00:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='RegressorProject',
     version='0.0.1',
-    author='Krissh',
-    author_email='krish.naik@ineuron.ai',
+    author='Vivek',
+    author_email='vivekshriwas12911@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 )
